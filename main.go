e700159b3da482fcefaf084f431708e63695566a
Hans Moog <hm@mkjc.net>
2019-02-05 21:26:39 +0100

    Refactor: changed indetation to spaces of main.go

--- main.go
+++ main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	"github.com/iotadevelopment/go/plugins/cli"
-	"github.com/iotadevelopment/go/plugins/config"
-	"github.com/iotadevelopment/go/plugins/gossip"
-	"github.com/iotadevelopment/go/plugins/statusscreen"
-	"github.com/iotadevelopment/go/plugins/tangle"
-	"github.com/iotadevelopment/go/packages/ixi"
+    "github.com/iotadevelopment/go/plugins/cli"
+    "github.com/iotadevelopment/go/plugins/config"
+    "github.com/iotadevelopment/go/plugins/gossip"
+    "github.com/iotadevelopment/go/plugins/statusscreen"
+    "github.com/iotadevelopment/go/plugins/tangle"
+    "github.com/iotadevelopment/go/packages/ixi"
 )
 
 func main() {
-	ixi.Load(
-		// allow the node to be configured through a config file
-		config.PLUGIN,
+    ixi.Load(
+        // allow the node to be configured through a config file
+        config.PLUGIN,
 
-		// allow the node to be configure via cli parameters
-		cli.PLUGIN,
+        // allow the node to be configure via cli parameters
+        cli.PLUGIN,
 
-		// add the gossip layer
-		gossip.PLUGIN,
+        // add the gossip layer
+        gossip.PLUGIN,
 
-		// add persistence layer
-		tangle.PLUGIN,
+        // add persistence layer
+        tangle.PLUGIN,
 
-		// show a status screen while the node is running
-		statusscreen.PLUGIN,
-	)
+        // show a status screen while the node is running
+        statusscreen.PLUGIN,
+    )
 
-	ixi.Run()
+    ixi.Run()
 }
